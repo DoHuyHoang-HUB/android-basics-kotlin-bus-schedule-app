53903e4fd2b4568388593724f732c4b363fb9246
Đỗ Huy Hoàng <huyhoang14901@gmail.com>
2022-01-17 14:31:08 +0700

    connect db, create dao

--- app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
+++ app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
@@ -1,9 +1,11 @@
-package com.example.busschedule.schedule
+package com.example.busschedule.database.schedule
 
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import org.jetbrains.annotations.NotNull
 
+@Entity
 data class Schedule(
     @PrimaryKey val id: Int,
     @NotNull @ColumnInfo(name = "stop_name") val stopName: String,
--- app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -1,12 +1,13 @@
-package com.example.busschedule.schedule
+package com.example.busschedule.database.schedule
 
 import androidx.room.Dao
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ScheduleDao {
     @Query("select * from schedule order by arrival_time ASC")
-    fun getAll(): List<Schedule>
+    fun getAll(): Flow<List<Schedule>>
     @Query("select * from schedule where stop_name = :stopName order by arrival_time ASC")
-    fun getByStopName(stopName: String): List<Schedule>
+    fun getByStopName(stopName: String): Flow<List<Schedule>>
 }
